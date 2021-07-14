0e934e86f7d879c5de194d3eda226b34f7eee156
Gautham J <gauthamastro@gmail.com>
2021-07-14 19:36:35 +0530

    Updates

---
+++ precache-manifest.e56cb375e2af9c3bf827687b710fc6ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2e1e1a7f767c70c06f94ca349f5c39e",
+    "url": "/polkadex-second-tranche/index.html"
+  },
+  {
+    "revision": "2f60d5bef51c2e66f3b6",
+    "url": "/polkadex-second-tranche/static/css/main.2c1f067f.chunk.css"
+  },
+  {
+    "revision": "4367058c2020b106d225",
+    "url": "/polkadex-second-tranche/static/js/2.7a6da735.chunk.js"
+  },
+  {
+    "revision": "2f60d5bef51c2e66f3b6",
+    "url": "/polkadex-second-tranche/static/js/main.8f38dac0.chunk.js"
+  },
+  {
+    "revision": "08cf904e256b68db9e88",
+    "url": "/polkadex-second-tranche/static/js/runtime-main.36c2cff9.js"
+  }
+]);
